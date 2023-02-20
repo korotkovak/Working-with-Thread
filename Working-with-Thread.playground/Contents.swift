c6744392ca56d6286310f1d354559e14aed5cfe0
korotkovak <kristinakorotkova059@gmail.com>
2023-02-20 20:29:15 +0800

    Creating generatingThread and workThread

--- Working-with-Thread.playground/Contents.swift
+++ Working-with-Thread.playground/Contents.swift
@@ -1,3 +1,73 @@
-import UIKit
+import Foundation
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let condition = NSCondition()
+var availables = false
+var storageForChip: [Chip] = []
+
+public struct Chip {
+    public enum ChipType: UInt32 {
+        case small = 1
+        case medium
+        case big
+    }
+
+    public let chipType: ChipType
+
+    public static func make() -> Chip {
+        guard let chipType = Chip.ChipType(rawValue: UInt32(arc4random_uniform(3) + 1)) else {
+            fatalError("Incorrect random value")
+        }
+
+        return Chip(chipType: chipType)
+    }
+
+    public func sodering() {
+        let soderingTime = chipType.rawValue
+        sleep(UInt32(soderingTime))
+    }
+}
+
+class GeneratingThread: Thread {
+    override func main() {
+        for _ in 1...10 {
+            condition.lock()
+
+            storageForChip.append(Chip.make())
+            availables = true
+
+            condition.signal()
+            condition.unlock()
+
+            Thread.sleep(forTimeInterval: 2)
+        }
+    }
+}
+
+class WorkThread: Thread {
+    override func main() {
+        for _ in 1...10 {
+
+            while(!availables) {
+                condition.wait()
+            }
+
+            if let chip = storageForChip.first {
+                chip.sodering()
+                storageForChip.removeFirst()
+            }
+
+            if storageForChip.count < 1 {
+                availables = false
+            }
+        }
+    }
+}
+
+let generatingThread = GeneratingThread()
+generatingThread.start()
+
+let workThread = WorkThread()
+workThread.start()
